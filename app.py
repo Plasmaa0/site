2888c3ed54bb2dbfd03e8f3d6ca96a9440d0db2a
Plasmaa0 <plasmaa0@gmail.com>
2021-08-14 01:24:08 +0300

    .

--- app.py
+++ app.py
@@ -3,30 +3,30 @@ import json
 
 app = Flask(__name__, static_folder='static')
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET'])
+@app.route('/index', methods=['GET'])
 def home():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about', methods=['GET'])
 def about():
     return render_template('about.html')
 
-@app.route('/projects')
+@app.route('/projects', methods=['GET'])
 def projects():
     with open('jsons/projects.json', 'r') as f:
         projects = json.load(f)
         f.close()
     return render_template('projects.html', projects=projects)
 
-@app.route('/contacts')
+@app.route('/contacts', methods=['GET'])
 def contacts():
     with open('jsons/contacts.json', 'r') as f:
         contacts = json.load(f)
         f.close()
     return render_template('contacts.html', contacts=contacts)
 
-@app.route('/projects/<int:id>')
+@app.route('/projects/<int:id>', methods=['GET'])
 def project(id):
     with open('jsons/projects.json', 'r') as f:
         projects = json.load(f)
